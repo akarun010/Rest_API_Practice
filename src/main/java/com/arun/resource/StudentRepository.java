6481a603c8528e8bf7208ee39f488d58ae8b57f3
Arun <akarunff0@gmail.com>
2026-09-05 19:18:47 +0530

    Chapter 13 Completed

--- src/main/java/com/arun/resource/StudentRepository.java
+++ src/main/java/com/arun/resource/StudentRepository.java
@@ -1,19 +1,24 @@
 package com.arun.resource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StudentRepository {
-    List<Student> students = null;
+    List<Student> students = new ArrayList<>();
     public StudentRepository() {
         Student s1 = new Student(); s1.setId(1); s1.setName("Arun"); s1.setEmail("akarunff90@gmail.com");
         Student s2 = new Student(); s2.setId(2); s2.setName("Harsh"); s2.setEmail("harsh567@gmail.com");
         Student s3 = new Student(); s3.setId(3); s3.setName("Kavya"); s3.setEmail("kavikaviya@gmail.com");
         Student s4 = new Student(); s4.setId(4); s4.setName("Arya"); s4.setEmail("akaryakumar89@gmail.com");
         Student s5 = new Student(); s5.setId(5); s5.setName("Sowmiya"); s5.setEmail("chachowsomiya@gmail.com");
-        students = Arrays.asList(s1, s2, s3, s4, s5);
-    }
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
 
+    }
     public List<Student> getStudents(){
         return students;
     }
@@ -24,5 +29,10 @@ public class StudentRepository {
             }
         }
         return null;
+    }
+
+    public List<Student> createStudent(Student student) {
+        students.add(student);
+        return students;
     }
 }
--- src/main/java/com/arun/resource/StudentResource.java
+++ src/main/java/com/arun/resource/StudentResource.java
@@ -1,20 +1,24 @@
 package com.arun.resource;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import java.util.Arrays;
 import java.util.List;
 
 @Path("/students")
 public class StudentResource {
+    StudentRepository repo = new StudentRepository();
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Student> getStudents(){
-        StudentRepository repo = new StudentRepository();
         List<Student> students = repo.getStudents();
+        return students;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public List<Student> createStudent(Student student){
+        List<Student> students = repo.createStudent(student);
         return students;
     }
 
@@ -22,7 +26,6 @@ public class StudentResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{id}")
     public Student getStudentById(@PathParam("id") int id){
-        StudentRepository repo = new StudentRepository();
         Student student = repo.getStudentById(id);
         return student;
     }

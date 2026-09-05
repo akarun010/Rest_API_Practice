e4f206099980d4372a1cf37ee68b21a44ea1988e
Arun <akarunff0@gmail.com>
2026-09-05 18:37:41 +0530

    Chapter 8 Completed

---
+++ src/main/java/com/arun/resource/Student.java
@@ -0,0 +1,43 @@
+package com.arun.resource;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Student {
+    int id;
+    String name;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/arun/resource/StudentResource.java
+++ src/main/java/com/arun/resource/StudentResource.java
@@ -2,14 +2,28 @@ package com.arun.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
-@Path("/student")
+@Path("/students")
 public class StudentResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getMessage(){
-        return  "Student API Working!";
+    public List<Student> getStudents(){
+        StudentRepository repo = new StudentRepository();
+        List<Student> students = repo.getStudents();
+        return students;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Student getStudentById(@PathParam("id") int id){
+        StudentRepository repo = new StudentRepository();
+        Student student = repo.getStudentById(id);
+        return student;
     }
 }
